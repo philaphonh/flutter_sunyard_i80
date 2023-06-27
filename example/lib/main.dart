d434d674b162a6e60888c4e81a29c0f42c1fbf64
Arty <atyinthavongsa@gmail.com>
2023-06-27 11:44:46 +0700

    Add simple implementation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:flutter/services.dart';
-import 'package:flutter_sunyard_i80/flutter_sunyard_i80.dart';
+
+import 'package:sunyard_i80/sunyard_i80.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,35 +15,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _flutterSunyardI80Plugin = FlutterSunyardI80();
+  bool isPrinterAvailable = false;
+
+  String serialNumber = "";
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    initPage();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _flutterSunyardI80Plugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+  void initPage() async {
+    final isTrue = await Printer.isPrinterAvailable();
+    serialNumber = await TerminalInfo.serialNumber;
+    if (isTrue) {
+      setState(() {
+        isPrinterAvailable = isTrue;
+      });
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -54,10 +42,131 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text('Printer Serial Number: $serialNumber'),
+              ),
+              Center(
+                child: Text('Printer status: ${printerStatus()}'),
+              ),
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                      onPressed: () {
+                        printText();
+                      },
+                      child: const Text('Print Text')),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                      onPressed: () {
+                        printImage();
+                      },
+                      child: const Text('Print Image')),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                      onPressed: () {
+                        printBarcode();
+                      },
+                      child: const Text('Print Barcode')),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                      onPressed: () {
+                        printQr();
+                      },
+                      child: const Text('Print QR')),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  String printerStatus() {
+    return isPrinterAvailable ? "Available" : "Unavailable";
+  }
+
+  void printText() async {
+    try {
+      await Printer.appendText(
+        text: "Flutter",
+        textAlign: PrinterAlign.left,
+      );
+
+      await Printer.appendText(
+        text: "Flutter",
+        textAlign: PrinterAlign.right,
+      );
+
+      await Printer.appendText(
+        text: "Flutter",
+        textAlign: PrinterAlign.left,
+        isBoldFont: true,
+      );
+      await Printer.appendText(
+        text: "Flutter",
+        textAlign: PrinterAlign.right,
+        isLineBreak: false,
+        fontSize: PrinterFontSize.thirtySix,
+      );
+
+      await Printer.startPrint();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  void printImage() async {
+    // An image must have white background
+    final assetImage = await rootBundle.load("assets/wojak.jpg");
+
+    final bytes = Uint8List.view(assetImage.buffer);
+
+    await Printer.appendImage(
+      byteArray: bytes,
+      align: PrinterAlign.right,
+    );
+
+    await Printer.appendImage(
+      byteArray: bytes,
+      align: PrinterAlign.left,
+      sampleSize: 2,
+    );
+
+    await Printer.startPrint();
+  }
+
+  void printBarcode() async {
+    await Printer.appendBarCode(data: "1234567890");
+
+    await Printer.startPrint();
+  }
+
+  void printQr() async {
+    await Printer.appendQrCode(
+      data: "https://artyboy.dev",
+      width: 100,
+      height: 100,
+    );
+
+    await Printer.startPrint();
   }
 }
